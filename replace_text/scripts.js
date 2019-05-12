00d67e9c3a0e8a6c9c7a22a5b5d67e62078b9168
Jmackas <jamesrossmackay@gmail.com>
2019-05-12 15:38:38 +0800

    Comments

--- replace_text/scripts.js
+++ replace_text/scripts.js
@@ -1,9 +1,14 @@
 
-var all = document.getElementsByTagName("*");
+// Search through all HTML nodes
+let all = document.getElementsByTagName("*");
 
+// Loop through the results
 for (let i = 1; i < all.length; i++) {
+    // Add the node to an array
     var str = all[i].innerHTML;
+    // replace text
     var res = str.replace("webinar", "hello");
+    // output the replaced results to all nodes
     all[i].innerHTML = res;
 }
 
