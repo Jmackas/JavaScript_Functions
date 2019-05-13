67adcadc0da8a4e0d70974d0154b5a4c49b54a54
James Mackay <jamesrossmackay@gmail.com>
2019-05-13 08:25:50 +0800

    On change function working

--- replace_all_text/moodle_survey/scripts.js
+++ replace_all_text/moodle_survey/scripts.js
@@ -1,21 +1,27 @@
-let selection = document.querySelector("#s2id_235-jsn-uf-form-field .select2-choice span").innerHTML;
+document.getElementById("235-jsn-uf-form-field").onchange = function() {
+    invokeWordingChange()
+}
+;
 
-console.log(selection);
+function invokeWordingChange() {
 
+    let selection = document.querySelector("#s2id_235-jsn-uf-form-field .select2-choice span").innerHTML;
 
+    console.log("Worked!");
 
-if(selection = "Masters") {
+    if (selection == "Bachelors") {
+
         // Search through all HTML nodes
-    let all = document.getElementsByTagName("*");
+        let all = document.getElementsByTagName("*");
 
-    // Loop through the results
-    for (let i = 1; i < all.length; i++) {
-        // Add the node to an array
-        var str = all[i].innerHTML;
-        // replace text
-        var res = str.replace("webinar", "hello");
-        // output the replaced results to all nodes
-        all[i].innerHTML = res;
+        // Loop through the results
+        for (let i = 1; i < all.length; i++) {
+            // Add the node to an array
+            var str = all[i].innerHTML;
+            // replace text
+            var res = str.replace("webinar", "hello");
+            // output the replaced results to all nodes
+            all[i].innerHTML = res;
+        }
     }
 }
-
