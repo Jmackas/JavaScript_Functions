3cbe5d4832ecb5b35edc91e23c60299ce7ad6dea
James Mackay <jamesrossmackay@gmail.com>
2019-05-13 08:14:43 +0800

    Selector identified

--- replace_all_text/moodle_survey/scripts.js
+++ replace_all_text/moodle_survey/scripts.js
@@ -1,14 +1,21 @@
+let selection = document.querySelector("#s2id_235-jsn-uf-form-field .select2-choice span").innerHTML;
 
-// Search through all HTML nodes
-let all = document.getElementsByTagName("*");
+console.log(selection);
 
-// Loop through the results
-for (let i = 1; i < all.length; i++) {
-    // Add the node to an array
-    var str = all[i].innerHTML;
-    // replace text
-    var res = str.replace("webinar", "hello");
-    // output the replaced results to all nodes
-    all[i].innerHTML = res;
+
+
+if(selection = "Masters") {
+        // Search through all HTML nodes
+    let all = document.getElementsByTagName("*");
+
+    // Loop through the results
+    for (let i = 1; i < all.length; i++) {
+        // Add the node to an array
+        var str = all[i].innerHTML;
+        // replace text
+        var res = str.replace("webinar", "hello");
+        // output the replaced results to all nodes
+        all[i].innerHTML = res;
+    }
 }
 
