c94969d7fe5a9a14cab435ff8893bc94b24c5915
James Mackay <jamesrossmackay@gmail.com>
2019-05-13 09:46:59 +0800

    Webinat to Tutorial and vice versa completed

--- replace_all_text/moodle_survey/scripts.js
+++ replace_all_text/moodle_survey/scripts.js
@@ -4,22 +4,47 @@ document.getElementById("235-jsn-uf-form-field").onchange = function() {
 ;
 
 function invokeWordingChange() {
-
     let selection = document.querySelector("#s2id_235-jsn-uf-form-field .select2-choice span").innerHTML;
 
-    if (selection == "Bachelors" || selection == "Masters") {
-        console.log("Worked!");
-        // Search through all HTML nodes
-        let all = document.getElementsByTagName("*");
+    webinarToTutorial()
+    tutorialToWebinar()
 
-        // Loop through the results
-        for (let i = 1; i < all.length; i++) {
-            // Add the node to an array
-            var str = all[i].innerHTML;
-            // replace text
-            var res = str.replace("webinar", "hello");
-            // output the replaced results to all nodes
-            all[i].innerHTML = res;
+    function webinarToTutorial() {
+
+        if (selection == "Bachelors" || selection == "Masters") {
+            console.log("Worked!");
+            // Search through all HTML nodes
+            let webinars = document.querySelectorAll(".controls p, .control-label");
+
+            // Loop through the results
+            for (let i = 1; i < webinars.length; i++) {
+                // Add the node to an array
+                var str = webinars[i].innerHTML;
+                // replace text
+                var res = str.replace("webinar", "tutorial");
+                // output the replaced results to all nodes
+                webinars[i].innerHTML = res;
+            }
         }
     }
+
+    function tutorialToWebinar() {
+
+        if (selection == "Graduate Certificate" || selection == "Advanced Diploma" || selection == "Diploma") {
+            console.log("Worked!");
+            // Search through all HTML nodes
+            let webinars = document.querySelectorAll(".controls p, .control-label");
+
+            // Loop through the results
+            for (let i = 1; i < webinars.length; i++) {
+                // Add the node to an array
+                var str = webinars[i].innerHTML;
+                // replace text
+                var res = str.replace("tutorial", "webinar");
+                // output the replaced results to all nodes
+                webinars[i].innerHTML = res;
+            }
+        }
+    }
+
 }
